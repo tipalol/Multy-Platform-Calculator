c3ac0f5d971d68a9ba89de775c1e23db27bd632c
sorokindmitrij <tipalol2@gmail.com>
2019-07-24 20:47:02 +0500

    Added basic buttons and events for them
    Added buttons: Plus, Minus, Devide, Multiply, Enter and Keypad buttons (0, 1, 2 and etc.)
    Added events: for operation buttons it is {Button Name}_Pressed and have parameters: (object sender, EventArgs args). For Keypad buttons event called KeyPadButton_Pressed and have parameter (uint index) that will a number entered by pressed button

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Button Plus, Minus, Devide, Multiply, Enter;
+
+        Button[] KeyPad;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Operation buttons
+            Plus = new Button() { Text = "Plus" };
+            Minus = new Button() { Text = "Minus" };
+            Devide = new Button() { Text = "Devide" };
+            Multiply = new Button() { Text = "Multiply" };
+            Enter = new Button() { Text = "Enter" };
+
+            Plus.Pressed += Plus_Pressed;
+            Minus.Pressed += Minus_Pressed;
+            Devide.Pressed += Devide_Pressed;
+            Multiply.Pressed += Multiply_Pressed;
+            Enter.Pressed += Enter_Pressed;
+
+            //Keypad buttons
+            KeyPad = new Button[10];
+            for (uint index = 0; index < KeyPad.Count(); ++index)
+            {
+                ref var button = ref KeyPad[index];
+                button = new Button() { Text = index.ToString() };
+                button.Pressed += (o, e) => { KeyPadButton_Pressed(index); };
+            }
+        }
+
+        #region Keypad buttons
+        private void KeyPadButton_Pressed(uint index)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Operation buttons
+        //////////////////////////////////////////////////////
+        //Operations buttons
+        private void Enter_Pressed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Multiply_Pressed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Devide_Pressed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Minus_Pressed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Plus_Pressed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        //////////////////////////////////////////////////////
+        #endregion
+    }
+}
